f58bdf4d39fbce43b670ef601343a497f7c40c9e
Jan Skála <j.skala@hotmail.com>
2025-01-18 20:21:46 +0100

    Fix blog username format in git profile configuration

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -192,7 +192,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: 'skala-jg', // to hide blog section, keep it empty
+    username: 'skala.jg', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
